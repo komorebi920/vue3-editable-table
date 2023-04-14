348fbd9c5fc540a9f71f9d889b829c40cd790fdb
komorebi920 <yxc900920@163.com>
2023-04-15 07:06:47 +0800

    表格渲染和事件触发的数据采集

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,84 @@
 <template>
-  <MyTable />
+  <MyTable :data="tableData" @submit="editData" />
 </template>
 
-<script>
-export default {
-  name: "App",
-  components: {},
+<script setup>
+import { ref } from "vue";
+
+const tableData = ref({
+  tHead: [
+    {
+      key: "id",
+      text: "学号",
+      editable: false,
+    },
+    {
+      key: "name",
+      text: "姓名",
+      editable: false,
+    },
+    {
+      key: "age",
+      text: "年龄",
+      editable: false,
+    },
+    {
+      key: "chinese",
+      text: "语文",
+      editable: true,
+    },
+    {
+      key: "math",
+      text: "数学",
+      editable: true,
+    },
+    {
+      key: "english",
+      text: "英语",
+      editable: true,
+    },
+  ],
+  tBody: [
+    {
+      id: 1,
+      name: "张三",
+      age: 16,
+      chinese: 121,
+      math: 98,
+      english: 138,
+    },
+    {
+      id: 2,
+      name: "李四",
+      age: 17,
+      chinese: 91,
+      math: 147,
+      english: 118,
+    },
+    {
+      id: 3,
+      name: "王五",
+      age: 18,
+      chinese: 129,
+      math: 139,
+      english: 141,
+    },
+  ],
+});
+
+const editData = ({ index, key, value, text }) => {
+  if (tableData.value.tBody[index][key] !== value) {
+    const confirm = window.confirm(`
+      您确定要将『${tableData.value.tBody[index].name}』的『${tableData.value.tBody[index][text]}』成绩修改为『${value}』分吗？
+    `);
+
+    if (confirm) {
+      tableData.value.tBody = tableData.value.tBody.map((item, idx) => {
+        index === idx && (item[key] = value);
+        return item;
+      });
+    }
+  }
 };
 </script>
 
--- src/modules/my-ui/MyTable/MyTable.vue
+++ src/modules/my-ui/MyTable/MyTable.vue
@@ -1,7 +1,62 @@
 <template>
-  <h1>MyTable</h1>
+  <table border="1" class="my-table">
+    <thead>
+      <tr>
+        <th v-for="item of tHead" :key="item.key">
+          {{ item.text }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, idx) of tBody" :key="item.id">
+        <td
+          v-for="(value, key) in item"
+          :key="key"
+          @click="showEditInput($event, key, idx)"
+        >
+          {{ value }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineProps, defineEmits, toRefs } from "vue";
 
-<style lang="scss" scoped></style>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+    default: () => ({
+      tHead: [],
+      tBody: [],
+    }),
+  },
+});
+
+defineEmits(["submit"]);
+
+const { tHead, tBody } = toRefs(props.data);
+
+function showEditInput(evt, key, idx) {
+  console.log(evt, key, idx);
+}
+</script>
+
+<style lang="scss" scoped>
+.my-table {
+  width: 100%;
+  border-collapse: collapse;
+
+  tr {
+    height: 44px;
+
+    th,
+    td {
+      text-align: center;
+      cursor: pointer;
+    }
+  }
+}
+</style>
